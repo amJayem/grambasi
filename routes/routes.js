cd3c7c7f6fc6130e4367a20eeee15435d23c8c9c
amJayem <csejayem19@gmail.com>
2023-04-18 22:09:31 +0600

    users route fixed

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const addUser = require("./addUser");
-const allUsers = require("./allUsers");
-const deleteUser = require("./deleteUser");
-const addEvent = require("./Event/addEvent");
+const express = require('express')
+const router = express.Router()
+const addUser = require('./Users/addUser')
+const allUsers = require('./Users/allUsers')
+const deleteUser = require('./Users/deleteUser')
+const addEvent = require('./Event/addEvent')
 
-router.route("/add-user").post(addUser);
-router.route("/all-users").get(allUsers);
-router.route("/delete-user").delete(deleteUser);
+router.route('/add-user').post(addUser)
+router.route('/all-users').get(allUsers)
+router.route('/delete-user').delete(deleteUser)
 
-router.route("/add-event").post(addEvent);
+router.route('/add-event').post(addEvent)
 
-module.exports = router;
+module.exports = router
 // export default router
