eeb279fbed0d1e16418b03d2b94b0f6e798db1f8
amJayem <csejayem19@gmail.com>
2023-04-28 23:37:16 +0600

    balance removed when user is deleted

--- routes/Users/deleteUser.js
+++ routes/Users/deleteUser.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const userSchema = require('../../models/addUserModel')
+const balanceSchema = require('../../models/balanceModel')
 
 const deleteUser = router.delete('/delete-user', async (req, res) => {
   try {
     const { id } = req.query
-    const result = await userSchema.findByIdAndDelete(id)
-    res.status(201).send(result)
+    const deletedUser = await userSchema.findByIdAndDelete(id)
+    const filter = { id: id }
+    const deletedBalance = await balanceSchema.findOneAndDelete(filter)
+    res.status(201).send({ deletedUser, deletedBalance })
   } catch (error) {
     console.error(error)
     res.status(500).send({ message: error.message })
