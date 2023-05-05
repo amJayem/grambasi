0733209a7450c13831780afc7193a415f0699df5
siam50 <siamurrahman50@gmail.com>
2023-05-05 17:26:41 +0600

    credit post error solved

--- models/creditModel.js
+++ models/creditModel.js
@@ -1,19 +1,19 @@
 // add-balance
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Mongoose Schema
 const creditSchema = new mongoose.Schema(
   {
     creditNote: {
       type: String,
-      required: true
+      required: true,
     },
     credit: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
   { timestamps: true }
-)
+);
 
-module.exports = mongoose.model('credit', creditSchema)
+module.exports = mongoose.model("credit", creditSchema);
