6233de22cc70e4aa1ce6520822f32dab9b240880
amJayem <csejayem19@gmail.com>
2023-05-20 23:27:01 +0600

    notice api modified

--- routes/notice/addNotice.js
+++ routes/notice/addNotice.js
@@ -5,8 +5,15 @@ const noticeSchema = require('../../models/noticeModal')
 const addNotice = router.post('/add-notice', async (req, res) => {
   try {
     const data = req.body
-    const newNotice = new noticeSchema(data)
-    const noticeAdded = await newNotice.save()
+    // const newNotice = new noticeSchema(data)
+    const noticeAdded = await noticeSchema.findOneAndUpdate(
+      { _id: '6468fbe6ec46ffae9f50fcdc' },
+      data,
+      {
+        upsert: true,
+        new: true
+      }
+    )
     res.status(201).send(noticeAdded)
     console.log(noticeAdded)
     // res.send(result)
