9a0fe362beb3951c108dd85d676865e100a45749
Asraf <asraf.uddin4407@gmail.com>
2023-07-23 00:58:36 +0600

    remove required

--- models/addUserModel.js
+++ models/addUserModel.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const addUserSchema = new mongoose.Schema(
   {
     memberRule: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     role: {
       type: String,
@@ -16,48 +16,49 @@ const addUserSchema = new mongoose.Schema(
       required: true
     },
     image: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     fatherName: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     motherName: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     image: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     memberId: {
       type: Number,
       required: true
     },
     mobile: {
-      type: Number,
-      required: true
+      type: Number
+      // required: true
     },
     dateOfBirth: {
-      type: Date,
-      required: true
+      type: Date
+      // required: true
     },
     nomineeName: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     nomineeMobile: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     address: {
-      type: String,
-      required: true
+      type: String
+      // required: true
     },
     gender: {
-      type: String,
-      required: true
+      type: String
+      // required: false
+      // required: true
       // enum: ['expense', 'credit']
     }
   },
