ab73a6b9972e35ce0f72174efd4dba60a4f64774
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-07-24 13:05:38 +0600

    usermodel

--- models/addUserModel.js
+++ models/addUserModel.js
@@ -5,11 +5,9 @@ const addUserSchema = new mongoose.Schema(
 	{
 		memberRule: {
 			type: String,
-			// required: true,
 		},
 		role: {
 			type: String,
-			// required: true,
 		},
 		name: {
 			type: String,
@@ -17,15 +15,12 @@ const addUserSchema = new mongoose.Schema(
 		},
 		fatherName: {
 			type: String,
-			// required: true,
 		},
 		motherName: {
 			type: String,
-			// required: true,
 		},
 		image: {
 			type: String,
-			required: true,
 		},
 		memberId: {
 			type: String,
@@ -37,24 +32,19 @@ const addUserSchema = new mongoose.Schema(
 		},
 		dateOfBirth: {
 			type: Date,
-			// required: true,
 		},
 		nomineeName: {
 			type: String,
-			// required: true,
 		},
 		nomineeMobile: {
 			type: String,
-			// required: true,
 		},
 		address: {
 			type: String,
-			// required: true,
 		},
 		gender: {
 			type: String,
 			required: true,
-			// enum: ['expense', 'credit']
 		},
 	},
 	{ timestamps: true }
