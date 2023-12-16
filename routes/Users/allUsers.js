73181c64cd2ca4967517347de852fff434224ed1
amjayem <csejayem19@gmail.com>
2023-12-16 23:05:50 +0600

    pagination implemented

--- routes/Users/allUsers.js
+++ routes/Users/allUsers.js
@@ -4,10 +4,26 @@ const userSchema = require('../../models/addUserModel')
 
 const allUsers = router.get('/all-users', async (req, res) => {
   try {
-    const result = await userSchema.find()
-    // const result = await userSchema.find().limit(20).sort({ _id: -1 })
+    const page = parseInt(req?.query?.page) || 1 // Get the page number from the query parameter, default to 1
+    const pageSize = parseInt(req?.query?.pageSize) || 200 // Set the page size, default to 10
+
+    const skip = (page - 1) * pageSize // Calculate the number of documents to skip
+
+    // Fetch paginated users from the database
+    // const users = await User.find()
+    //   .skip(skip)
+    //   .limit(pageSize)
+    //   .exec();
+
+    const result = await userSchema.find().limit(pageSize) //.sort({ _id: -1 })
+    // Count all documents in the collection (without any query)
+    const totalCount = await userSchema.countDocuments()
+    console.log(totalCount)
+    const sortedDataMemberRole = result?.sort(
+      (a, b) => a?.memberRole?.id - b?.memberRole?.id
+    )
     // console.log(result)
-    res.status(201).send(result)
+    res.status(201).json({ sortedDataMemberRole, totalCount, page, pageSize })
   } catch (error) {
     // console.error(error)
     res.status(500).send({ message: error.message })
