b8ca3853d9c025faddcb681897855a942b3b3acf
amjayem <csejayem19@gmail.com>
2024-01-06 22:57:14 +0600

    lastInsertedAmount removed

--- routes/balance/getBalance.js
+++ routes/balance/getBalance.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const balanceModel = require('../../models/balanceModel')
-const lastInsertedAmount = require('../../models/lastInsertedAmountModel.js')
 const monthlyBalanceModel = require('../../models/monthlyBalanceModel.js')
 const router = express.Router()
 
@@ -15,14 +14,22 @@ const getBalance = router.get('/total-balance', async (req, res) => {
     res.status(500).send({ success: false, message: error.message })
   }
 })
+
 const getMonthlyBalance = router.get('/monthly-balance', async (req, res) => {
   try {
-    const result = await lastInsertedAmount.find()
+    const month = req.query.month
+    const year = req.query.year
+    console.log(`${month}-${year}`)
+    // Assuming you have a 'date' field in your schema to represent the month and year
+    const result = await monthlyBalanceModel.find({
+      // Assuming 'date' is a field in your schema representing the month and year
+      month: `${month}-${year}`
+    })
     res.status(200).send({ success: true, result })
   } catch (error) {
     res.status(500).send({ success: false, message: error.message })
   }
 })
-0
+
 module.exports = getBalance
 module.exports = getMonthlyBalance
