b66ed82fa790cb8cc55ded6961e9c7701962ad07
amjayem <csejayem19@gmail.com>
2024-01-06 23:29:28 +0600

    console log removed

--- routes/balance/getBalance.js
+++ routes/balance/getBalance.js
@@ -19,7 +19,6 @@ const getMonthlyBalance = router.get('/monthly-balance', async (req, res) => {
   try {
     const month = req.query.month
     const year = req.query.year
-    console.log(`${month}-${year}`)
     // Assuming you have a 'date' field in your schema to represent the month and year
     const result = await monthlyBalanceModel.find({
       // Assuming 'date' is a field in your schema representing the month and year
